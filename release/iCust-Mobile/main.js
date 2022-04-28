e01138f83cc150977dee6a450b8f5c8b070b125e
unknown <sneha.sontineni@rumango.com>
2022-04-28 20:57:55 +0530

    Mobile changes

--- release/iCust-Mobile/main.js
+++ release/iCust-Mobile/main.js
@@ -2216,8 +2216,7 @@ __webpack_require__.r(__webpack_exports__);
 const environment = {
     production: true,
     // ICUST_URL: 'http://localhost:1212', // phase2 for local testing
-    //ICUST_URL:'http://192.168.0.14:8081/Icust-Digital-Banking'
-    ICUST_URL: 'http://192.168.0.14:9001/Icust-Digital-Banking',
+    ICUST_URL: 'http://192.168.0.14:8081/Icust-Digital-Banking'
 };
 
 
